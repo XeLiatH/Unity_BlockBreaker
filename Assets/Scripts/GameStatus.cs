94fa9787a4ca2c4f8d6c2d37f944093af9aec938
Matěj Beran <matej.beran96@gmail.com>
2019-09-08 19:29:15 +0200

    Add score to the level canvas.

--- Assets/Scripts/GameStatus.cs
+++ Assets/Scripts/GameStatus.cs
@@ -1,17 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameStatus : MonoBehaviour
 {
     [Range(0.1f, 10f)] [SerializeField] float gameSpeed = 1f;
     [SerializeField] int pointsPerBlockDestroyed = 42;
+    [SerializeField] TextMeshProUGUI scoreText;
+
+    // state variables
     [SerializeField] int currentScore = 0;
+
+    void Start()
+    {
+        UpdateScore();
+    }
 
     // Update is called once per frame
     void Update()
     {
         Time.timeScale = gameSpeed;
+        UpdateScore();
+    }
+
+    private void UpdateScore()
+    {
+        scoreText.text = currentScore.ToString();
     }
 
     public void AddToScore()
