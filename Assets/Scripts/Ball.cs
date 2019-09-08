56706a40486bd6b03c36f2e4cf4cc71923692e21
Matěj Beran <matej.beran96@gmail.com>
2019-09-08 12:40:51 +0200

    Add ability to launch the Ball off the Paddle.

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -6,20 +6,42 @@ public class Ball : MonoBehaviour
 {
     // config params
     [SerializeField] Paddle paddle1;
+    [SerializeField] float xPush = 2f;
+    [SerializeField] float yPush = 15f;
 
     // state
     Vector2 paddleToBallVector;
+    bool isBallLaunched;
 
     // Start is called before the first frame update
     void Start()
     {
+        isBallLaunched = false;
         paddleToBallVector = transform.position - paddle1.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (!isBallLaunched)
+        {
+            LockBallToPaddle();
+            LaunchOnMouseClick();
+        }
+    }
+
+    private void LockBallToPaddle()
+    {
         Vector2 paddlePosition = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
         transform.position = paddlePosition + paddleToBallVector;
+    }
+
+    private void LaunchOnMouseClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            isBallLaunched = true;
+            GetComponent<Rigidbody2D>().velocity = new Vector2(xPush, yPush);
+        }
     }
 }
