31e2ff8a1f777a4d1b2d02e7168696aff992cab8
Matěj Beran <matej.beran96@gmail.com>
2019-09-08 12:12:33 +0200

    Limit paddle X movement to be within the screen.

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Paddle : MonoBehaviour
 {
     [SerializeField] float screenWidthInUnits = 16f;
+    [SerializeField] float minXInUnits = 1f;
+    [SerializeField] float maxXInUnits = 15f;
 
     // Start is called before the first frame update
     void Start()
@@ -16,7 +18,7 @@ public class Paddle : MonoBehaviour
     void Update()
     {
         float mousePositionInUnits = Input.mousePosition.x / Screen.width * screenWidthInUnits;
-        Vector2 paddlePosition = new Vector2(mousePositionInUnits, transform.position.y);
+        Vector2 paddlePosition = new Vector2(Mathf.Clamp(mousePositionInUnits, minXInUnits, maxXInUnits), transform.position.y);
         this.transform.position = paddlePosition;
     }
 }
