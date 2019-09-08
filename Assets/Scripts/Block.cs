20de4b700fc85e2520195b9e91d3005a528c1f16
Matěj Beran <matej.beran96@gmail.com>
2019-09-08 18:17:41 +0200

    Add sound when a block breaks.

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class Block : MonoBehaviour
 {
+    [SerializeField] AudioClip breakSoundClip;
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        AudioSource.PlayClipAtPoint(breakSoundClip, Camera.main.transform.position);
         Destroy(gameObject);
     }
 }
