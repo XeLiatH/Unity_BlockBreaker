e83b6380fa6d8bb0782a2741d5b201c31563e808
Matěj Beran <matej.beran96@gmail.com>
2019-09-08 17:31:25 +0200

    Play audio at random when ball collision occurs.

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -8,16 +8,21 @@ public class Ball : MonoBehaviour
     [SerializeField] Paddle paddle1;
     [SerializeField] float xPush = 2f;
     [SerializeField] float yPush = 15f;
+    [SerializeField] AudioClip[] ballSounds;
 
     // state
     Vector2 paddleToBallVector;
     bool hasGameStarted;
+
+    // cached component references
+    AudioSource audioSource;
 
     // Start is called before the first frame update
     void Start()
     {
         hasGameStarted = false;
         paddleToBallVector = transform.position - paddle1.transform.position;
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -49,7 +54,9 @@ public class Ball : MonoBehaviour
     {
         if (hasGameStarted)
         {
-            GetComponent<AudioSource>().Play();
+            int index = Random.Range(0, ballSounds.Length);
+            var clip = ballSounds[index];
+            audioSource.PlayOneShot(clip);
         }
     }
 }
