58734d8543d56fa51f4ea2f9c2cd521ece2dad09
Matěj Beran <matej.beran96@gmail.com>
2019-09-08 18:28:32 +0200

    Add ability to count breakable blocks.

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -6,6 +6,15 @@ public class Block : MonoBehaviour
 {
     [SerializeField] AudioClip breakSoundClip;
 
+    // cached reference
+    Level level;
+
+    void Start()
+    {
+        level = FindObjectOfType<Level>();
+        level.CountBreakableBlock();
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         AudioSource.PlayClipAtPoint(breakSoundClip, Camera.main.transform.position);
---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour
+{
+    [SerializeField] int breakableBlocks; // serialized for debugging purposes
+
+    
+
+    public void CountBreakableBlock()
+    {
+        breakableBlocks++;
+    }
+}
