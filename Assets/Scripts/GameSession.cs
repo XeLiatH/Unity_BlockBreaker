7f59eec9d8da04c6087503c400ad0d9cb6032ae5
Matěj Beran <matej.beran96@gmail.com>
2019-09-09 22:25:17 +0200

    Polishing existing stuff before adding more levels and going craaaazy.

--- Assets/Scripts/GameSession.cs
+++ Assets/Scripts/GameSession.cs
@@ -8,6 +8,7 @@ public class GameSession : MonoBehaviour
     [Range(0.1f, 10f)] [SerializeField] float gameSpeed = 1f;
     [SerializeField] int pointsPerBlockDestroyed = 42;
     [SerializeField] TextMeshProUGUI scoreText;
+    [SerializeField] bool isAutoplayEnabled = false;
 
     // state variables
     [SerializeField] int currentScore = 0;
@@ -51,5 +52,10 @@ public class GameSession : MonoBehaviour
     public void ResetGame()
     {
         Destroy(gameObject);
+    }
+
+    public bool IsAutoplayEnabled()
+    {
+        return isAutoplayEnabled;
     }
 }
--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -8,17 +8,30 @@ public class Paddle : MonoBehaviour
     [SerializeField] float minXInUnits = 1f;
     [SerializeField] float maxXInUnits = 15f;
 
+    GameSession gameSession;
+    Ball ball;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        gameSession = FindObjectOfType<GameSession>();
+        ball = FindObjectOfType<Ball>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float mousePositionInUnits = Input.mousePosition.x / Screen.width * screenWidthInUnits;
-        Vector2 paddlePosition = new Vector2(Mathf.Clamp(mousePositionInUnits, minXInUnits, maxXInUnits), transform.position.y);
+        Vector2 paddlePosition = new Vector2(Mathf.Clamp(GetXPos(), minXInUnits, maxXInUnits), transform.position.y);
         this.transform.position = paddlePosition;
+    }
+
+    private float GetXPos()
+    {
+        if (gameSession.IsAutoplayEnabled())
+        {
+            return ball.transform.position.x;
+        }
+
+        return Input.mousePosition.x / Screen.width * screenWidthInUnits;
     }
 }
